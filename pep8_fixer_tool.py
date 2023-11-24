06648e4394ba51ac9bf23081758240dccc19542d
Ivan Molina <47658617+BakuKurae@users.noreply.github.com>
2023-11-23 21:21:41 -0600

    Adding test files and created the main window of the app

--- pep8_fixer_tool.py
+++ pep8_fixer_tool.py
@@ -1,1 +1,33 @@
-#source code
+#source code: GUI app
+
+import tkinter
+from tkinter import filedialog as fd
+import os
+
+# main window settings
+main_w = tkinter.Tk()
+main_w.title("PEP8 auto-fixer")
+main_w.geometry("400x400")
+main_w.resizable(False,False)
+
+file_name = "--"
+
+def open_text_file():
+    filetypes = (
+        ('python files', '*.py'),
+        ('All files', '*.*')
+    )
+    path = fd.askopenfilename(filetypes=filetypes)
+    global file_name
+    file_name = os.path.basename(path)
+    file_label.config(text = file_name)
+
+# widgets
+file_button = tkinter.Button(main_w, text='Select file', command=open_text_file)
+file_button.place(x=10, y=10)
+check_button = tkinter.Button(main_w, text='Check')
+check_button.pack(side = 'bottom')
+file_label = tkinter.Label(main_w, text=file_name)
+file_label.place(x = 80, y=13)
+
+main_w.mainloop()
---
+++ test.py
@@ -0,0 +1,23 @@
+# Demo Python File with PEP 8 Violations
+# Feel free to use this file to test the auto-fixer
+
+def bad_function(x):
+    y=x*2
+    if y>10:
+        print("Result is greater than 10.")
+    else:
+        print("Result is less than or equal to 10.")
+        
+class BadClass:
+    def __init__(self, name):
+        self.name=name
+
+    def display_info(self):
+        print(f"Name:{self.name}")
+
+def main():
+    obj=BadClass("Demo")
+    obj.display_info()
+    
+if __name__ == "__main__":
+    main()
