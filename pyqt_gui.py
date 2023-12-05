a4b1c6537bca38e4e6b346c7e2af577e1a4b59dc
Ivan Molina <47658617+BakuKurae@users.noreply.github.com>
2023-12-05 02:46:17 -0600

    Adding ignore list to formatting process and constraints to enable the fix button

--- pyqt_gui.py
+++ pyqt_gui.py
@@ -1,14 +1,17 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QAbstractItemView
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QAbstractItemView, QDialog
 from PySide6.QtCore import *
 from ui_mainwindow import Ui_MainWindow
 import os
 import pycodestyle
+import autopep8
 import re
 from io import StringIO
 
 file_name = ""
-codes = []
+path = ""
+codes = {}
+ignore = []
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -20,10 +23,25 @@ class MainWindow(QMainWindow):
         self.ui.fixButton.setEnabled(False)
         self.ui.browseButton.clicked.connect(self.browseFile)
         self.ui.checkButton.clicked.connect(self.checkFile)
-        self.ui.listWidget.addItems(codes)
+        self.ui.markButton.clicked.connect(self.markAll)
+        self.ui.fixButton.clicked.connect(self.selectionChanged)
         self.ui.listWidget.setSelectionMode(QAbstractItemView.MultiSelection)
+        self.ui.listWidget.itemClicked.connect(self.selectionChanged)
+        self.ui.listWidget.itemDoubleClicked.connect(self.detailsItem)
+        self.ui.progressBar.setValue(100)
+        self.ui.progressBar.setVisible(False)
+
+    def detailsItem(self):
+        print("Details")
+
+    def selectionChanged(self):
+        if len(self.ui.listWidget.selectedItems()) > 0:
+            self.ui.fixButton.setEnabled(True)
+        else:
+            self.ui.fixButton.setEnabled(False)
 
     def browseFile(self):
+        global path
         path, _ = QFileDialog.getOpenFileName(self, 
                                                   'Single File', 
                                                   QDir.rootPath(), 
@@ -32,11 +50,12 @@ class MainWindow(QMainWindow):
         file_name = os.path.basename(path)
         self.ui.lineEdit.setText(file_name)
         self.ui.checkButton.setEnabled(True)
-        print(file_name)
     
     def checkFile(self):
         global codes
-        codes = []
+        codes = {}
+        global ignore
+        ignore = []
         old_stdout = sys.stdout
         sys.stdout = stdout_data = StringIO()
         file_checker = pycodestyle.Checker(file_name, show_source=False)
@@ -44,10 +63,45 @@ class MainWindow(QMainWindow):
         sys.stdout = old_stdout
         print("Found %s errors (and warnings)" % file_errors)
         for code in stdout_data.getvalue().split("\n")[:-1]:
-            codes.append(re.findall("(?<=: )(.{4})", code)[0])
+            print(code)
+            key = re.findall("(?<=: )(.{4})", code)[0]
+            if key in codes:
+                codes[key].append(re.findall("(?<=:)(.*?)(?=:)", code)[0])
+            else:
+                ignore.append(key)
+                codes[key] = []
+
         self.ui.listWidget.clear()
-        self.ui.listWidget.addItems(codes)
-        print(codes)
+        self.ui.listWidget.addItems(codes.keys())
+        if len(codes) > 0:
+            self.ui.markButton.setEnabled(True)
+            # self.ui.fixButton.setEnabled(True)
+        else:
+            self.success()
+    
+    def markAll(self):
+        self.ui.listWidget.selectAll()
+    
+    def fix(self):
+        global ignore
+        for item in self.ui.listWidget.selectedItems():
+            ignore.pop(ignore.index(item.text()))
+        print(ignore)
+        file_data = open(path, "r")
+        data = file_data.read()
+        fixed_data = autopep8.fix_code(data, options={'ignore': ignore})
+        prefix_path = re.sub(file_name, "", path)
+        prefix_file = "fixed_"
+        fixed_file = open(f"{prefix_path}{prefix_file}{file_name}", "w")
+        fixed_file.write(fixed_data)
+        fixed_file.close()
+        print("Fixed!")
+
+    def success(self):
+        dlg = QDialog(self)
+        dlg.setWindowTitle("Fixed!")
+        dlg.setFixedSize(300, 100)
+        dlg.exec()
 
 
 if __name__ == "__main__":
